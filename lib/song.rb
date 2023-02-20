9b24bb4f9d75d5ab43a7c7799bce90891aaf9626
Isaac Ongera <isaacongera717@gmail.com>
2023-02-20 13:50:50 +0300

    completed assignment

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song
+    attr_accessor :name, :artist, :genre
+
+    def initialize(name, artist, genre)
+       @name = name
+       @artist = artist
+       @genre = genre 
+       @@count += 1
+       @@genres << @genre
+       @@artists << @artist
+    end
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+end
